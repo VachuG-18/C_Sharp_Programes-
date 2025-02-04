491bb62f73fba32f51df2ce26b3ae263b386f55a
VATCHALA G <105353640+VachuG-18@users.noreply.github.com>
2025-02-04 19:54:12 +0530

    Add files via upload
    nill

---
+++ Simple Calculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator Using C#");
+            Console.WriteLine("Emter the 1st Number:");
+            double num1=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the character like (+,-,*,/)");
+            char opp = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the 2nd Number:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            switch (opp)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    if (num2 != 0) 
+                    
+                        result = num1 / num2; 
+                    else
+                    
+                        Console.WriteLine("zero Division Error!");
+                    break;
+
+                default:
+                    Console.WriteLine("The invalid Output");
+                    return;
+
+            }
+            Console.WriteLine($"{num1}{opp}{num2}= {result}");
+
+
+        }
+    }
+}
